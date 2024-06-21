97f1feff04e03b23ee1f90047f2cf2d057173ef8
Adewole-Gege <michaelgegs@gmail.com>
2024-06-20 20:05:00 -0700

    corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,18 @@
 #include <unistd.h>   /* for write function */
 #include <stdlib.h>   /* for EXIT_FAILURE */
-
+/**
+* main - Entry point of the program
+*
+* Description: Writes a specific quote to standard error using the write system call.
+*              The quote is from Dora Korpar, dated October 19, 2015.
+* Return: EXIT_FAILURE (indicating an unsuccessful termination of the program)
+*/
 int main(void) {
-    /* Message to be written to stderr */
-    char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    
-    /* Write the quote to stderr using write system call */
-    write(STDERR_FILENO, quote, 59);  /* 59 is the length of the string excluding the null terminator */
-    
-    return EXIT_FAILURE;  /* returning EXIT_FAILURE (which is defined as 1) as per the requirement */
+/* Message to be written to stderr */
+char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+/* Write the quote to stderr using write system call */
+write(STDERR_FILENO, quote, 59);  /* 59 is the length of the string excluding the null terminator */
+
+return EXIT_FAILURE;  /* returning EXIT_FAILURE (which is defined as 1) as per the requirement */
 }
