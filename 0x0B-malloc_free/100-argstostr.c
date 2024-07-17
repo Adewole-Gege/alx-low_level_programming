c6f6caa0d29c672d2ce9690fd59e522121e7563b
Adewole-Gege <michaelgegs@gmail.com>
2024-07-16 20:30:13 -0700

    omo

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,54 +1,48 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * argstostr - concatenates all the arguments of your program.
- * @ac: argument count
- * @av: argument vector
- *
- * Return: pointer to a new string, or NULL if it fails
- */
+* argstostr - concatenates all the arguments of your program.
+* @ac: argument count
+* @av: argument vector
+*
+* Return: pointer to a new string, or NULL if it fails
+*/
 char *argstostr(int ac, char **av)
 {
-    char *str;
-    int i, j, len, total = 0;
-    int newline_count = ac - 1; /* Number of newlines needed */
-
-    if (ac == 0 || av == NULL)
-        return (NULL);
-
-    /* Calculate total length of concatenated string */
-    for (i = 0; i < ac; i++)
-    {
-        len = 0;
-        while (av[i][len] != '\0')
-        {
-            len++;
-            total++;
-        }
-    }
-
-    /* Add space for newlines */
-    total += newline_count;
-
-    /* Allocate memory for concatenated string + null terminator */
-    str = malloc(sizeof(char) * (total + 1));
-    if (str == NULL)
-        return (NULL);
-
-    /* Copy each argument followed by newline */
-    total = 0;
-    for (i = 0; i < ac; i++)
-    {
-        j = 0;
-        while (av[i][j] != '\0')
-        {
-            str[total++] = av[i][j++];
-        }
-        /* Add newline after each argument */
-        if (i < ac - 1)
-            str[total++] = '\n';
-    }
-    str[total] = '\0'; /* Null terminator */
-
-    return (str);
+char *str;
+int i, j, len, total = 0;
+int newline_count = ac - 1; /* Number of newlines needed */
+if (ac == 0 || av == NULL)
+return (NULL);
+/* Calculate total length of concatenated string */
+for (i = 0; i < ac; i++)
+{
+len = 0;
+while (av[i][len] != '\0')
+{
+len++;
+total++;
+}
+}
+/* Add space for newlines */
+total += newline_count;
+/* Allocate memory for concatenated string + null terminator */
+str = malloc(sizeof(char) * (total + 1));
+if (str == NULL)
+return (NULL);
+/* Copy each argument followed by newline */
+total = 0;
+for (i = 0; i < ac; i++)
+{
+j = 0;
+while (av[i][j] != '\0')
+{
+str[total++] = av[i][j++];
+}
+/* Add newline after each argument */
+if (i < ac - 1)
+str[total++] = '\n';
+}
+str[total] = '\0'; /* Null terminator */
+return (str);
 }
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,41 +1,39 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * str_concat - concatenates two strings
- * @s1: first string
- * @s2: second string
- *
- * Return: pointer to a newly allocated space in memory with concatenated string
- *         NULL on failure
- */
+* str_concat - Concatenates two strings.
+* @s1: The first string.
+* @s2: The second string.
+*
+* Return: If concatenation fails - NULL.
+*         Otherwise - a pointer to the newly-allocated space in memory
+*                    containing the concatenated strings.
+*/
 char *str_concat(char *s1, char *s2)
 {
-	char *concat;
-	int len1 = 0, len2 = 0;
-	int i, j;
+char *concat_str;
+int i, j, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-	/* Calculate lengths of s1 and s2 */
-	while (s1[len1])
-		len1++;
-	while (s2[len2])
-		len2++;
+while (s1[len1] != '\0')
+len1++;
+while (s2[len2] != '\0')
+len2++;
 
-	/* Allocate memory for concatenated string (+1 for null terminator) */
-	concat = malloc((len1 + len2 + 1) * sizeof(char));
-	if (concat == NULL)
-		return (NULL);
+concat_str = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	/* Copy s1 and s2 to concat */
-	for (i = 0; i < len1; i++)
-		concat[i] = s1[i];
-	for (j = 0; j < len2; j++)
-		concat[len1 + j] = s2[j];
-	concat[len1 + len2] = '\0'; /* Null terminate the concatenated string */
+if (concat_str == NULL)
+return (NULL);
 
-	return (concat);
+for (i = 0; i < len1; i++)
+concat_str[i] = s1[i];
+
+for (j = 0; j <= len2; j++)
+concat_str[i + j] = s2[j];
+
+return (concat_str);
 }
