c3c078de2b22bbbe5cafb706d3d70d82254a01d3
Adewole-Gege <michaelgegs@gmail.com>
2024-07-22 15:44:52 -0700

    yup

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,12 @@
+# 0x0E-structures_typedef
+
+## Tasks
+
+### 0. Define a new type struct dog
+
+Create a header file that defines a new type `struct dog` with the following elements:
+- `name`, type = `char *`
+- `age`, type = `float`
+- `owner`, type = `char *`
+
+**File:** `dog.h`
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - a structure that stores a dog's basic info
+* @name: name of the dog (type = char *)
+* @age: age of the dog (type = float)
+* @owner: owner of the dog (type = char *)
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif /* DOG_H */
