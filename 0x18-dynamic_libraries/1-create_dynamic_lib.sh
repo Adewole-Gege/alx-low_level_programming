fcec60d1786c5b3fbf6d1a0d2ea046e6fe867284
Adewole-Gege <michaelgegs@gmail.com>
2024-12-24 13:28:44 +0100

    0 corrections

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-# Remove old object files and the existing dynamic library
-rm -f *.o libdynamic.so
 
-# Compile only the necessary .c files into the dynamic library, excluding dynamic_lib.c
-gcc -shared -o libdynamic.so -fPIC $(ls *.c | grep -v "dynamic_lib.c" | grep -v "main.c" | grep -v "0-main.c")
+# Compile specific .c files for the dynamic library
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -fPIC -c _putchar.c _abs.c _strlen.c dynamic_lib.c
+
+# Create the dynamic library
+gcc -shared -o libdynamic.so _putchar.o _abs.o _strlen.o dynamic_lib.o
+
+# Clean up object files
+rm *.o
